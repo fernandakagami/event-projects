2a89b86dc08ebca78576bc53cefdf002cbcb15d8
Fernanda <fernandakagami@gmail.com>
2022-09-23 15:18:38 -0300

    update carousel

--- dio-multiverse/assets/scripts/script.js
+++ dio-multiverse/assets/scripts/script.js
@@ -19,3 +19,14 @@ function addEventListenersToCards() {
 }
 
 document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+
+function selectCarouselItem(selectedButtonElement) {
+    const selectedItem = selectedButtonElement.id;
+    const carousel = document.querySelector('.s-cards-carousel');
+    const transform = carousel.style.transform;
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) - 1);
+    const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+    carousel.style.transform = newTransform;
+}
