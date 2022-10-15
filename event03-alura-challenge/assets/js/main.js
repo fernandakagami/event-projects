e3c9ea8f072ea3d363ac831731493494b3a0bda6
Fernanda <fernandakagami@gmail.com>
2022-10-15 07:04:04 -0300

    update js functions

--- event03-alura-challenge/assets/js/main.js
+++ event03-alura-challenge/assets/js/main.js
@@ -1,28 +1,37 @@
 let loadMore = document.querySelectorAll(".loadMoreButton");
-let products = document.querySelectorAll(".product");
 let sizeWindow = parseInt(window.innerWidth);
 
-if (sizeWindow >= 1000) {  
-  let fullSize = document.querySelectorAll(".no-show");
-  for (let index = 0; index < 6; index++) {
-    fullSize[index].classList.add("show");
-    fullSize[index].classList.remove("no-show");    
-  } 
-  for (let i = 0; i < loadMore.length; i++) {
-    loadMore[i].addEventListener("click", function() {
-      let showing = products[i].querySelectorAll(".no-show");
-      console.log(showing);
-      for (let index = 6; index < showing.length; index++) {
-        showing[index].classList.add("show");
-        showing[index].classList.remove("no-show");
+function sizeOfWindow(sizeWindow) {  
+  let products = document.querySelectorAll(".product");  
+  for (let index = 0; index < products.length; index++) {
+    let product = products[index].querySelectorAll(".no-show");     
+    if (sizeWindow >= 1000) {    
+      for (let index = 0; index < 6; index++) {
+        product[index].classList.add("show");
+        product[index].classList.remove("no-show");    
       }
-      loadMore[i].remove();
-    });     
-  }    
-} else {
-  for (let i = 0; i < loadMore.length; i++) {
-    loadMore[i].addEventListener("click", function() {
-      let showing = products[i].querySelectorAll(".no-show");
+    } else if (sizeWindow >= 700) {
+      for (let index = 0; index < 4; index++) {                    
+          product[index].classList.add("show");
+          product[index].classList.remove("no-show");
+      }    
+    } else {
+      for (let index = 0; index < 2; index++) {
+          product[index].classList.add("show");
+          product[index].classList.remove("no-show"); 
+      }
+    }    
+  }
+}
+
+sizeOfWindow(sizeWindow);
+
+
+function moreProduct() {  
+  let products = document.querySelectorAll(".product"); 
+  for (let i = 0; i < loadMore.length; i++) {  
+    loadMore[i].addEventListener("click", function() {      
+      let showing = products[i].querySelectorAll(".no-show");    
       console.log(showing);
       for (let index = 0; index < showing.length; index++) {
         showing[index].classList.add("show");
@@ -30,8 +39,11 @@ if (sizeWindow >= 1000) {
       }
       loadMore[i].remove();
     });     
-  }  
+  }    
 }
+
+moreProduct();
+
 
 var coll = document.querySelector(".collapsible");
 
